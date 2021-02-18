79be2abef4a23691e0c1d554b754ce8422ea93ed
yaocs2 <yaocs2@midea.com>
2021-02-18 19:13:53 +0800

    spring-cloud-test> Eureka: EurekaServer启动测试

---
+++ eureka/eureka-server/src/main/java/com/jsonyao/springcloud/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.jsonyao.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server测试启动类
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
