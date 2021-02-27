6781af11527672b42a77567fde604d72af08975b
yaocs2 <yaocs2@midea.com>
2021-02-27 23:46:32 +0800

    spring-cloud-test> Config: 配置中心化的配置中心: 测试拉取GitHub上的配置文件

---
+++ config/config-server/src/main/java/com/jsonyao/springcloud/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package com.jsonyao.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 注册中心: 可拉取配置文件
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+
+    //    获取配置文件的不同URL姿势，都是GET请求, 如果不指定{label}的话默认用master
+    // 1. http://localhost:60000/{label}/{application}-{profile}.* (yml, properties, json)
+    // 2. http://localhost:60000/{application}/{profile}/{label}
+}
