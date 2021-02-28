cc3f240b5ed149de9d14ecfa38e53e80977665b1
yaocs2 <yaocs2@midea.com>
2021-02-28 11:59:39 +0800

    spring-cloud-test> Config: 配置配置中心的客户端: 测试动态刷新配置文件

---
+++ config/config-client/src/main/java/com/jsonyao/springcloud/controller/ConfigClientRefreshController.java
@@ -0,0 +1,32 @@
+package com.jsonyao.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 配置中心客户端: 动态刷新配置文件
+ */
+@RestController
+@RequestMapping("/refresh")
+// 运行期进行刷新这个Bean, 在下一次方法调用时会对所有上下游依赖重新注入
+@RefreshScope
+public class ConfigClientRefreshController {
+
+    /**
+     * 测试配置中心动态刷新到本地配置文件的配置属性
+     */
+    @Value("${myWords}")
+    private String words;
+
+    /**
+     * 测试配置中心动态刷新到本地配置文件的配置属性
+     * @return
+     */
+    @GetMapping("/words")
+    public String getWords(){
+        return words;
+    }
+}
